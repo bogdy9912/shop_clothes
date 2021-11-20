44749941be02152755d422dc30d80f3a20048007
Bogdan Marin <bogdancons99@gmail.com>
2021-11-20 12:45:33 +0200

    add node

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs"); // Or `import fs from "fs";` with ESM
 
+const {Client} = require("pg");
 const ip = require("ip");
 console.dir(ip.address());
 const app = express();
 
 const port = 8080;
+
+var client=new Client({ user: 'bogdan', password:'bogdan', database:'simple_properties', host:'localhost', port:5432 });
 
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'app')));
@@ -17,6 +20,37 @@ app.get('/', (req, res) => {
     res.render('pagini/index');
 });
 
+
+
+
+app.get('/produse', function (req, res) {
+    let conditie = "";
+    if ("tip" in req.query){
+        conditie=` and tip_produs='${req.query.tip}'`;
+    }
+    client.query(`select * from products where 1=1 ${conditie}`, function (err, rez){
+        if (!err){
+            console.log(rez);
+            // res.render();
+        }else{}
+    });
+})
+app.get('/produs/:id', function (req, res) {
+    let {id} = req.param();
+    let conditie = "";
+    if ("tip" in req.query){
+        conditie=` and tip_produs='${req.query.tip}'`;
+    }
+    client.query(`select * from products where  id=${id}`, function (err, rez){
+        if (!err){
+            console.log(rez);
+            if (rez.rows){
+            // res.render('pagini/produs', {prod:rez.row[0]});
+            }
+            //res.render('eroooare');
+        }else{}
+    });
+})
 // app.get('/index', (req, res) => {
 //
 //     res.render('pagini/index');
